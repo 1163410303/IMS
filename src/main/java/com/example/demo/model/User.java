d56fe5dbeaacbaa691c671f576352176d8b3df4a
Guangyuan <wanggy03@inspur.com>
2020-08-24 09:20:06 +0800

    init project

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,51 @@
+package com.example.demo.model;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private int type;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", type=" + type +
+                '}';
+    }
+}
